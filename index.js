1184eeee576a83c4ebf65dcb542e81f781d721c2
Sadman kabir <sadmankabir351@gmail.com>
2025-12-11 18:54:35 +0600

    cors required

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3000
 
+app.use(cors())
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/hello', (req, res) => {
+  res.send('Hello from /hello endpoint!')
+
 })
 
 app.listen(port, () => {
